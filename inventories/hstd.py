4c2b089f9e2868b9062dab19d14e6d71997fb61b
Gleb Ivanov <glivanov@samokat.ru>
2026-03-20 01:00:30 +0300

    Convert into a pyinfra proj

---
+++ inventories/hstd.py
@@ -0,0 +1,7 @@
+hosts = [
+    ("192.168.2.50", {
+        "ssh_user": "gleb",
+        "_sudo": True,
+        "wpa_psk": "f3fd69f2dfb5d0a73449dfc2fb2a9a8b6000d65f8e31522bf41ad97eb7d9e7d6"
+    }),
+]
