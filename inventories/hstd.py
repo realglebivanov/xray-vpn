aaa0f44a5e508fbce424499abfd4552600d8ee94
Gleb Ivanov <realglebivanov@gmail.com>
2026-03-21 02:14:59 +0300

    Extract interface names into pyinfra vars

--- inventories/hstd.py
+++ inventories/hstd.py
@@ -2,6 +2,10 @@ hosts = [
     ("192.168.2.50", {
         "ssh_user": "gleb",
         "_sudo": True,
-        "wpa_psk": "REMOVED"
+        "wpa_psk": "REMOVED",
+        "wan_dev": "eno1",
+        "apd_dev": "wlp4s0",
+        "lan_dev": "enp2s0",
+        "tun_dev": "xray0",
     }),
 ]
--- tasks/configs.py
+++ tasks/configs.py
@@ -1,11 +1,15 @@
 from pyinfra.operations import files
 from deploy.triggers import notify
 
-notify("systemd-networkd", files.sync(
-    name="Sync /etc/systemd/network",
-    src="templates/network",
-    dest="/etc/systemd/network/",
-    mode="0644", user="root", group="root"))
+for src, dest in [
+    ("templates/network/10-apd.network.j2", "/etc/systemd/network/10-apd.network"),
+    ("templates/network/20-wan.network.j2", "/etc/systemd/network/20-wan.network"),
+    ("templates/network/30-lan.network.j2", "/etc/systemd/network/30-lan.network"),
+]:
+    notify("systemd-networkd", files.template(
+        name=f"Deploy {dest}",
+        src=src, dest=dest,
+        mode="0644", user="root", group="root"))
 
 notify("nftables", files.template(
         name="Deploy /etc/nftables.conf",
@@ -41,16 +45,16 @@ for d in ["/srv/navidrome/music", "/srv/navidrome/data", "/srv/navidrome/cache"]
     notify("navidrome", files.directory(
         name=f"Create {d}", path=d, mode="0755", user="navidrome", group="navidrome"))
 
-notify("networkd-dispatcher", files.put(
+notify("networkd-dispatcher", files.template(
     name="Deploy networkd-dispatcher routable.d script",
-    src="templates/networkd-dispatcher-routable",
+    src="templates/networkd-dispatcher-routable.j2",
     dest="/etc/networkd-dispatcher/routable.d/xrayvpnd",
     mode="0755", user="root", group="root"))
 
 for state in ["no-carrier", "off", "degraded"]:
-    notify("networkd-dispatcher", files.put(
+    notify("networkd-dispatcher", files.template(
         name=f"Deploy networkd-dispatcher {state}.d script",
-        src="templates/networkd-dispatcher-no-carrier",
+        src="templates/networkd-dispatcher-no-carrier.j2",
         dest=f"/etc/networkd-dispatcher/{state}.d/xrayvpnd",
         mode="0755", user="root", group="root"))
 
@@ -60,7 +64,18 @@ notify("nftables", files.directory(
     mode="0755", user="root", group="root"))
 
 notify("nftables", files.put(
-    name="Deploy nftables-override.conf",
+    name="Deploy nftables override.conf",
     src="templates/nftables-override.conf",
     dest="/etc/systemd/system/nftables.service.d/override.conf",
+    mode="0644", user="root", group="root"))
+
+notify("xrayvpnd", files.directory(
+    name="Create tun2socksd.service.d",
+    path="/etc/systemd/system/tun2socksd.service.d",
+    mode="0755", user="root", group="root"))
+
+notify("xrayvpnd", files.template(
+    name="Deploy tun2socksd override.conf",
+    src="templates/tun2socksd-override.conf.j2",
+    dest="/etc/systemd/system/tun2socksd.service.d/override.conf",
     mode="0644", user="root", group="root"))
