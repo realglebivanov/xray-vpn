d1cf0486c0bfe2e36ef9b6555a0d0d67f2c70a4c
Gleb Ivanov <glivanov@samokat.ru>
2026-03-21 02:14:59 +0300

    Extract interface names into pyinfra vars

--- inventories/hstd.py
+++ inventories/hstd.py
@@ -2,6 +2,10 @@ hosts = [
     ("192.168.2.50", {
         "ssh_user": "gleb",
         "_sudo": True,
-        "wpa_psk": "f3fd69f2dfb5d0a73449dfc2fb2a9a8b6000d65f8e31522bf41ad97eb7d9e7d6"
+        "wpa_psk": "f3fd69f2dfb5d0a73449dfc2fb2a9a8b6000d65f8e31522bf41ad97eb7d9e7d6",
+        "wan_dev": "eno1",
+        "apd_dev": "wlp4s0",
+        "lan_dev": "enp2s0",
+        "tun_dev": "xray0",
     }),
 ]
